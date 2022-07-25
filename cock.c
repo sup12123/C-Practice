d94c85262cdaa9d45df009d98d501922b40022b8
sup12123 <95585537+sup12123@users.noreply.github.com>
2022-07-25 19:21:03 +0000

    Create cock.c

---
+++ cock.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// This software is FOSS as God intended
+int main() {
+printf("Hello World\n Do whatever the fuck you want with my shitty software/n>
+
+
+
+/*benis in bagina
+Boonis in bagoona*/
+
+float penis = 69.420;
+char cock = 'd';
+
+printf("%c\n", cock);
+printf("%f\n", penis);
+return 0;
+}
+
+
+
+
+
+
+
